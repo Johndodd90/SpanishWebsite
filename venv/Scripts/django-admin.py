9846eda4e87230171aab41ddb96e178da0fb71e5
John Dodd <johndodd90@gmail.com>
2020-06-15 17:33:27 -0500

    First commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\john\desktop\python specialist path\django\spanishwebsite\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
