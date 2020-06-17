01d80ea5132bdbbd651f5ca0ce4651bab53bf268
John Dodd <johndodd90@gmail.com>
2020-06-17 16:24:14 -0500

    functionality to add new blog post and display on home page as list

--- spanish/blog/views.py
+++ spanish/blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/index.html', context)
 
 
 def about(request):
