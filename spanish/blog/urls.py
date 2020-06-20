2b870217581927860e42deeb082b63b6e62912a7
John Dodd <johndodd90@gmail.com>
2020-06-20 15:51:40 -0500

    Create blog post functionality added

--- spanish/blog/urls.py
+++ spanish/blog/urls.py
@@ -1,8 +1,10 @@
-
 from django.urls import path
-
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', PostListView.as_view(), name='index'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about', views.about, name='about'),
 ]
--- spanish/blog/views.py
+++ spanish/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
-
-# Create your views here.
 
 
 def index(request):
@@ -10,6 +10,26 @@ def index(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/index.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
